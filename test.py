a60e11f561b7484d6b3392f67fe01460215c25a4
Kyle Hoefker <hoefker.2@osu.edu>
2023-09-08 20:41:21 -0400

    comments

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch # pip install elasticsearch (I had to manually move the libraries to this directory)
 import logging
 
 
 es = Elasticsearch("https://localhost:9200",
-                    basic_auth=('elastic', 'VXQLGUfva56soBM7fH4R'),
-                    ca_certs='http_ca.crt',
-                    verify_certs=False)
+                    basic_auth=('elastic', 'VXQLGUfva56soBM7fH4R'), # my ELASTIC_PASSWORD
+                    ca_certs='http_ca.crt', # file must be in this directory
+                    verify_certs=False) # source of the warnings, mimics '-k' flag
 
 if es.ping():
     print('Yay Connect')
