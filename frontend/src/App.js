471ac4070916615b528b72d0d8f96012e1947e9d
Aidan Pukansky <70171173+deweydec@users.noreply.github.com>
2023-09-19 15:40:27 -0400

    added button from mui library meant for testing mui library install with docker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="contained">Contained</Button>
       </header>
     </div>
   );
