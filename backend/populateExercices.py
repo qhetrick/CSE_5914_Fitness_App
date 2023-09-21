88033a57ef61c7db349b321577931160e4e6bb4d
kHoefker <kylehoefker@gmail.com>
2023-09-21 17:19:37 -0400

    Cleaned up test.py (--> populateData.py) and added requirements.txt

--- backend/populateExercices.py
+++ backend/populateExercices.py
@@ -5,6 +5,9 @@ import logging
 import csv
 import os
 
+# Ignore warnings for now
+logging.captureWarnings(True)
+
 # Connect to elastic search
 esPass = 'VXQLGUfva56soBM7fH4R' # my ELASTIC_PASSWORD
 es = Elasticsearch("https://localhost:9200",
@@ -19,7 +22,8 @@ else:
     print('Awww it could not connect!')
 
 # Create index
-os.system(f'curl -k --cacert ../Resources/http_ca.crt -u elastic:{esPass} -X PUT "https://localhost:9200/exercises')
+es.indices.delete(index="exercises")
+es.indices.create(index="exercises")
 
 # Populate data
 with open('../Resources/exercise_list.csv') as csv_file:
@@ -30,6 +34,15 @@ with open('../Resources/exercise_list.csv') as csv_file:
         if line_count == 0:
             tags = row
         else:
-            jsonEntry = f'{{""{tags[0]}"":""{row[0]}"",""{tags[1]}"":""{row[1]}"",""{tags[2]}"":""{row[2]}"",""{tags[3]}"":""{row[3]}"",""{tags[4]}"":""{row[4]}"",""{tags[5]}"":""{row[5]}"",""{tags[6]}"":""{row[6]}""}}'
-            os.system(f'curl -k --cacert ../Resources/http_ca.crt -u elastic:{esPass} -X POST https://localhost:9200/exercises/_doc/{row[0]} -H "Content-Type: application/json" -d "{jsonEntry}"')
+            jsonEntry = f'{{"{tags[0]}":"{row[0]}","{tags[1]}":"{row[1]}","{tags[2]}"":""{row[2]}"",""{tags[3]}"":""{row[3]}"",""{tags[4]}"":""{row[4]}"",""{tags[5]}"":""{row[5]}"",""{tags[6]}"":""{row[6]}""}}'
+            jsonEntry = {
+                f'{tags[0]}': f'{row[0]}',
+                f'{tags[1]}': f'{row[1]}',
+                f'{tags[2]}': f'{row[2]}',
+                f'{tags[3]}': f'{row[3]}',
+                f'{tags[4]}': f'{row[4]}',
+                f'{tags[5]}': f'{row[5]}',
+                f'{tags[6]}': f'{row[6]}',
+            }
+            es.create(index="exercises", id=row[0], document=jsonEntry)
         line_count += 1
