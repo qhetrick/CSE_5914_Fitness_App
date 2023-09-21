0e76217b3160363ab62a364d0bb154600e5a4aad
Ohenness <hennessey.64@osu.edu>
2023-09-21 13:19:05 -0400

    Display CSV

---
+++ frontend/src/CSVDisplay.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react';
+import Papa from 'papaparse';
+import Data from './exercise_list.csv'
+
+// This function is used to populate an object that contains a video that will be displayed on the web page
+function CSVDisplay() {
+    const [data, setData] = useState([]);
+
+    // parse CSV data and store it in the component state
+    // const handleFileUpload = (event) => {
+    //   const file = event.target.files[0];
+    //   Papa.parse(file, {
+    //     header: true,
+    //     complete: (results) => {
+    //       setData(results.data);
+    //     },
+    //   });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(Data);
+            const reader = response.body.getReader();
+            const result = await reader.read();
+            const decoder = new TextDecoder("utf-8");
+            const csvData = decoder.decode(result.value);
+            const parsedData = Papa.parse(csvData, {
+                header: true,
+                skipEmptyLines: true
+            }).data;
+            setData(parsedData);
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div className="App">
+
+            {/* <input type="file" accept=".csv" onChange={handleFileUpload} /> */}
+
+            {data.length ? (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>name</th>
+                            <th>equipment</th>
+                            <th>level</th>
+                            <th>muscle</th>
+                            <th>previewSrc</th>
+                            <th>videoLink</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((row, index) => (
+                            <tr key={index}>
+                                <td>{row.id}</td>
+                                <td>{row.name}</td>
+                                <td>{row.equipment}</td>
+                                <td>{row.level}</td>
+                                <td>{row.muscle}</td>
+                                <td>{row.previewSrc}</td>
+                                <td>{row.videoLink}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : null}
+
+            <br /><br />
+
+        </div>
+    );
+}
+
+export default CSVDisplay;
