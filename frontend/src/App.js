d9637f1cea52c2d0256451016e56ea381d1fb8dc
Aidan Pukansky <70171173+deweydec@users.noreply.github.com>
2023-09-21 15:37:55 -0400

    Created navbar for home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,43 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { createTheme } from '@mui/material/styles';
 import './App.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#6a1b9a',
+      dark: '#6a1b9a'
+    },
+
+    secondary: {
+      main: '#6a1b9a',
+      dark: '#6a1b9a'
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload muthaaaa.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="contained">Contained</Button>
-      </header>
+    <div className="App-header" >
+        <Box sx={{ flexGrow: 1 }} >
+          <AppBar  theme={theme} color="primary" position="static">
+            <Toolbar>
+              <Typography sx={{ p: 1 }} variant='h5'>FitFriend</Typography>
+              <Button color="inherit">Home</Button>
+              <Button color="inherit">Button1</Button>
+              <Button color="inherit">Button2</Button>
+              <Button color="inherit">About</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
     </div>
+
+    
   );
 }
 
