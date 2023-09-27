6305f03fae86699333544ac1194969027a5e9889
Ohenness <hennessey.64@osu.edu>
2023-09-27 13:58:01 -0400

    Combined HomePage and Owen branches into main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,39 @@
 import logo from './logo.svg';
-import Button from '@mui/material/Button';
 import './App.css';
 import './VideoCard.css';
 import CSVDisplay from './CSVDisplay';
+import { Box, AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f45d05',
+      dark: '#f45d05'
+    },
+
+    secondary: {
+      main: '#f45d05',
+      dark: '#f45d05'
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload muthaaaa.
-        </p>
-        <br></br>
-        <Button variant="contained" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </Button>
-        <br />
-        <Button href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel="noreferrer" >
-          Click me
-        </Button>
-      </header>
+    <div className='App'>
+        <Box sx={{ flexGrow: 1 }} >
+          <AppBar  theme={theme} color='primary' position='static'>
+            <Toolbar>
+              <Typography sx={{ p: 1 }} variant='h5'>FitFriend</Typography>
+              <Button color='inherit'>Home</Button>
+              <Button color='inherit'>Button1</Button>
+              <Button color='inherit'>Button2</Button>
+              <Button color='inherit'>About</Button>
+              <Button color='inherit'href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer' >Click me</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
       <CSVDisplay />
     </div>
   );
