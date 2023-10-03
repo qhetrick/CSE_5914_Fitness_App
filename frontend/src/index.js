e3b173c30e1994da14df57319a61c7febd7b0078
Aidan Pukansky <70171173+deweydec@users.noreply.github.com>
2023-10-03 15:21:05 -0400

    Fixed css import for index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/index.css';
 import reportWebVitals from './reportWebVitals';
 import { Pages } from './pages/Pages';
 
