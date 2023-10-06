c0908426d2cc71fc8a27a59d6ccb19685535a1d9
kHoefker <kylehoefker@gmail.com>
2023-10-06 13:48:09 -0400

    cleanup

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from elasticsearch import Elasticsearch
 import logging
-import os
 import sys
+import populateData
 
 # Ignore security warnings for now
 logging.captureWarnings(True)
@@ -11,18 +11,18 @@ esPass = sys.argv[1]
 
 # Poplulate the data
 print('Populating Data...')
-os.system(f'python populateData.py {esPass}')
+populateData.populateData(esPass)
 print('Done!')
 
 # Connect to elastic search
 es = Elasticsearch("https://localhost:9200",
                     basic_auth=('elastic', esPass),
-                    ca_certs='../http_ca.crt', # file must be in this directory
+                    ca_certs='../Resources/http_ca.crt', # file must be in this directory
                     verify_certs=False) # source of the warnings, mimics '-k' flag
 
 # Run basic searches
 if not es.ping():
-    print('ERROR: Coulnd''t connect to Elasticsearch.')
+    print('ERROR: Couldn''t connect to Elasticsearch.')
 else:
     userInput = False
     while userInput != 'q':
@@ -32,3 +32,5 @@ else:
             print("ID, Name, Equipment, Level, Muscle, Preview Source, Video Link")
             for hit in results['hits']['hits']:
                 print("%(id)s, %(name)s, %(equipment)s, %(level)s, %(muscle)s, %(previewSrc)s, %(videoLink)s" % hit["_source"])
+
+
--- backend/populateData.py
+++ backend/populateData.py
@@ -1,46 +1,46 @@
-# wsl
-# sysctl -w vm.max_map_count=262144
-from elasticsearch import Elasticsearch # pip install elasticsearch (I had to manually move the libraries to this directory)
+from elasticsearch import Elasticsearch
 import logging
 import csv
-import sys
 
-# Ignore security warnings for now
-logging.captureWarnings(True)
 
-# Connect to elastic search
-esPass =  sys.argv[1] # ELASTIC_PASSWORD 'VXQLGUfva56soBM7fH4R'
-es = Elasticsearch("https://localhost:9200",
-                    basic_auth=('elastic', esPass),
-                    ca_certs='../http_ca.crt', # file must be in this directory
-                    verify_certs=False) # source of the warnings, mimics '-k' flag
+def populateData(esPass):
+    # Ignore security warnings for now
+    logging.captureWarnings(True)
 
-if es.ping():
-    print('Connected')
-else:
-    print('ERROR: Couldn''t connect.')
+    # Connect to elastic search
+    es = Elasticsearch("https://localhost:9200",
+                        basic_auth=('elastic', esPass),
+                        ca_certs='../Resources/http_ca.crt', # file must be in this directory
+                        verify_certs=False) # source of the warnings, mimics '-k' flag
 
-# Create index
-es.indices.delete(index="exercises")
-es.indices.create(index="exercises")
+    if es.ping():
+        print('Connected')
+    else:
+        print('ERROR: Couldn''t connect.')
 
-# Populate data
-with open('../Resources/exercise_list.csv') as csv_file:
-    csvReader = csv.reader(csv_file)
-    line_count = 0
-    tags = []
-    for row in csvReader:
-        if line_count == 0:
-            tags = row
-        else:
-            jsonEntry = {
-                f'{tags[0]}': f'{row[0]}',
-                f'{tags[1]}': f'{row[1]}',
-                f'{tags[2]}': f'{row[2]}',
-                f'{tags[3]}': f'{row[3]}',
-                f'{tags[4]}': f'{row[4]}',
-                f'{tags[5]}': f'{row[5]}',
-                f'{tags[6]}': f'{row[6]}',
-            }
-            es.create(index="exercises", id=row[0], document=jsonEntry)
-        line_count += 1
+    # Create index
+    try:
+        es.indices.delete(index="exercises")
+    finally:
+        es.indices.create(index="exercises")
+
+    # Populate data
+    with open('exercise_list.csv') as csv_file:
+        csvReader = csv.reader(csv_file)
+        line_count = 0
+        tags = []
+        for row in csvReader:
+            if line_count == 0:
+                tags = row
+            else:
+                jsonEntry = {
+                    f'{tags[0]}': f'{row[0]}',
+                    f'{tags[1]}': f'{row[1]}',
+                    f'{tags[2]}': f'{row[2]}',
+                    f'{tags[3]}': f'{row[3]}',
+                    f'{tags[4]}': f'{row[4]}',
+                    f'{tags[5]}': f'{row[5]}',
+                    f'{tags[6]}': f'{row[6]}',
+                }
+                es.create(index="exercises", id=row[0], document=jsonEntry)
+            line_count += 1
