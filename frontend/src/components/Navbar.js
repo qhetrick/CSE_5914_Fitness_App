31f2bd37ae7d4ebba827a07ecf15570d10f56197
Ohenness <hennessey.64@osu.edu>
2023-10-17 15:29:15 -0400

    Construction of new branch

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,19 +1,30 @@
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 export default function Navbar() {
-    const theme = useTheme();
+  const theme = useTheme();
 
-    return(
-        <AppBar  theme={theme} color='primary' position='static'>
-            <Toolbar>
-              <Typography sx={{ p: 1 }} variant='h5'>FitFriend</Typography>
-              <Button color='inherit'>Home</Button>
-              <Button color='inherit' href='/search' rel='noreferrer'>Search for Workout</Button>
-              <Button color='inherit'>Button Placeholder</Button>
-              <Button color='inherit'>About</Button>
-              <Button color='inherit'href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer' >Click me</Button>
-            </Toolbar>
-          </AppBar>
-    )
+  return (
+    <AppBar theme={theme} color="primary" position="static">
+      <Toolbar>
+        <Typography sx={{ p: 1 }} variant="h5">
+          FitFriend
+        </Typography>
+        <Button color="inherit">Home</Button>
+        <Button color="inherit" href="/search" rel="noreferrer">
+          Search for Workout
+        </Button>
+        <Button color="inherit">Button Placeholder</Button>
+        <Button color="inherit">About</Button>
+        <Button
+          color="inherit"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Click me
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
 }
