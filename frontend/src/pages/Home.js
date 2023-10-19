682192f73945a2d3a67adaad5f4bcd995d95d4ac
Ohenness <hennessey.64@osu.edu>
2023-10-19 14:08:26 -0400

    Homepage image working

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import "../css/App.css";
 import "../css/VideoCard.css";
 import { Box } from "@mui/material";
 import Navbar from "../components/Navbar";
+import FitFriend_Logo from "./FitFriend_Logo.jpeg";
 
 function Home() {
   return (
@@ -14,6 +15,9 @@ function Home() {
     >
       <Box sx={{ flexGrow: 1, boxShadow: 3 }}>
         <Navbar />
+      </Box>
+      <Box className="FitFriend_Logo" sx={{ flexGrow: 1 }}>
+        <img src={FitFriend_Logo} alt="FitFriend_Logo" />
       </Box>
     </div>
   );
