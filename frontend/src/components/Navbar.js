03caa6d46f1ef733de2fb4c36b7aba20e58ed445
Aidan Pukansky <70171173+deweydec@users.noreply.github.com>
2023-10-26 15:11:25 -0400

    Edited Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
       <Toolbar>
         <Typography sx={{ p: 1 }} variant="h5"> FitFriend </Typography>
         <Button color="inherit" href="/">Home</Button>
-        <Button color="inherit" href="/search" rel="noreferrer">Search for Workout</Button>
+        <Button color="inherit" href="/search" rel="noreferrer">Generate Workout</Button>
         <Button color="inherit" href="/exercise">Exercises</Button>
         <Button color="inherit" href="/about">About</Button>
       </Toolbar>
