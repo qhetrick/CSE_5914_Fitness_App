92ad1cc5cd0886a86d26b66204291bfae9916610
JJ Berrios <jjberrios@JJs-MacBook-Air-2.local>
2023-11-09 15:01:17 -0500

    Integration json on homepage in window

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,14 +5,48 @@ import { Box } from "@mui/material";
 import Navbar from "../components/Navbar";
 import FitFriend_Logo from "./FitFriend_Logo.jpeg";
 
+import React, { useState } from 'react';
+
+
 function Home() {
 
-  async function getAll(){
-    await fetch("http://localhost:5000/exercises").then(response =>{
-      console.log(response);
-      console.log(response.json())
-    });
+  const [exercises, setExercises] = useState([]);
+  const [showData, setShowData] = useState(false);
+
+  async function getAll() {
+    try {
+      // Fetch the data from the Flask server
+      const response = await fetch("http://localhost:5000/exercises");
+  
+      // Check if the response is ok (status code 200-299)
+      if (!response.ok) {
+        throw new Error('Network response was not ok: ' + response.statusText);
+      }
+  
+      // Wait for the promise to resolve and get the JSON data
+      const data = await response.json();
+      setExercises(data.results);
+      setShowData(true);
+    } catch (error) {
+      // Catch any errors that occur during the fetch operation
+      console.error('There has been a problem with your fetch operation:', error);
+    }
   }
+
+  // Fetch exercises when the component mounts
+
+
+  // CSS for the scrollable container
+  const scrollableContainerStyle = {
+    height: '400px',
+    overflowY: 'auto',
+    color: 'white', // White text color
+    border: '1px solid #ddd',
+    marginTop: '20px',
+    padding: '10px',
+    display: showData ? 'block' : 'none', // Only display when showData is true
+    fontFamily: 'monospace',
+  };
 
   return (
     <div className="Home">
@@ -34,6 +68,11 @@ function Home() {
           }}
         />
       </Box>
+
+      {/* Scrollable container for raw JSON data */}
+      <div style={scrollableContainerStyle}>
+        <pre>{JSON.stringify(exercises, null, 2)}</pre>
+      </div>
     </div>
   );
 }
